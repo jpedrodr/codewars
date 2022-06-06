b298bd192ece327395e975827fded251fb255445
jpedrodr <jpedrodr@hotmail.,com>
2022-06-06 20:08:03 +0100

    ViewBinding enabled List fragment added to layout

--- app/src/main/java/com/jpedrodr/codewars/app/ui/MainActivity.kt
+++ app/src/main/java/com/jpedrodr/codewars/app/ui/MainActivity.kt
@@ -2,12 +2,27 @@ package com.jpedrodr.codewars.app.ui
 
 import android.os.Bundle
 import com.jpedrodr.codewars.R
+import com.jpedrodr.codewars.app.ui.challenge.list.ChallengeListFragment
 import com.jpedrodr.codewars.commons.Tagged
+import com.jpedrodr.codewars.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity(), Tagged {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        setupFragment()
+    }
+
+    private fun setupFragment() {
+        supportFragmentManager.beginTransaction().run {
+            add(R.id.fragment_container, ChallengeListFragment())
+            commit()
+        }
     }
 }
